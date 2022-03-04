2e12a9eff039867e041ffaf25bfde010074b143d
ivan91simeonov <vanches91@abv.bg>
2022-03-04 18:33:00 +0200

    remove posts

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', getPosts);
 
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+document.querySelector('#posts').addEventListener('click' , deletePost)
+
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -29,4 +31,20 @@ function submitPost() {
       getPosts();
     })
     .catch((err) => console.log(err));
+}
+
+function deletePost (e) {
+    e.preventDefault();
+   
+    if(e.target.parentElement.classList.contains('delete')) {
+      const id = e.target.parentElement.dataset.id
+      if(confirm('Are you sure?')) {
+        http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed' , 'alert alert-success')
+          getPosts()
+        })
+        .catch(err => console.log(err))
+      }
+    }
 }
